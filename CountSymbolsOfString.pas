14a5e39f80160d4455bb3b7ced0ca45c8e54d089
Alexander <pchp250g8@tutamail.com>
2026-04-22 16:17:37 +0300

    Created project CountSymbolsOfString

---
+++ CountSymbolsOfString.pas
@@ -0,0 +1,18 @@
+﻿uses
+  System;
+begin
+  Console.WriteLine('Input a string');
+  var strLine := Console.ReadLine();
+  var nLen := strLine.Length;
+  for var i := 1 to nLen do
+  begin
+    var c := 0;
+    for var j := 1 to nLen do
+    begin
+      if (strLine[i] = strLine[j]) then
+        inc(c);              
+    end;
+    Console.WriteLine('The symbol {0} occurs {1} times', strLine[i], c);
+  end;
+  Console.Read();
+end.
